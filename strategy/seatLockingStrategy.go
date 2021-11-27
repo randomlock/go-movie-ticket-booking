7f68b9d9f458f570547e4738797244e9045aa31b
abdullah <abdullahh@lambdatest.com>
2021-11-27 17:31:12 +0530

    initial commit

--- strategy/seatLockingStrategy.go
+++ strategy/seatLockingStrategy.go
@@ -1,6 +1,7 @@
 package strategy
 
 import (
+    "sync"
     "time"
 
     "../models"
@@ -16,19 +17,26 @@ const MAX_LOCK_TIME_DURATION = time.Second * 120
 
 type InMemoryLocking struct {
     lockedSeats map[string]time.Time
+    *sync.Mutex
 }
 
 func (i *InMemoryLocking) LockSeat(seat *models.Seat) {
+    i.Mutex.Lock()
     i.lockedSeats[seat.Id] = time.Now()
-    seat.LockSeat()
+    i.Mutex.Unlock()
 }
 
 func (i *InMemoryLocking) UnLockSeat(seat *models.Seat) {
+    i.Mutex.Lock()
     delete(i.lockedSeats, seat.Id)
-    seat.FreeSeat()
+    i.Mutex.Unlock()
+
 }
 
 func (i InMemoryLocking) IsSeatLocked(seat models.Seat) bool {
+    if _, exists := i.lockedSeats[seat.Id]; !exists {
+        return false
+    }
     return int(time.Now().Sub(i.lockedSeats[seat.Id]).Seconds()) < int(MAX_LOCK_TIME_DURATION)
 }
 
